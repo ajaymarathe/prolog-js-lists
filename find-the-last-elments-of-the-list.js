bc9acc329547fcbca4f3c9aff469c34721d187df
Ajay Marathe <ma143ajay@gmail.com>
2021-05-01 15:38:59 +0530

    added and fixed find the last elements of the list

---
+++ find-the-last-elments-of-the-list.js
@@ -0,0 +1,9 @@
+// (*) find the last elements of the list.
+
+function FindLastElement(list){
+    let lastElement = list[list.length - 1];
+    console.log('last elements:',lastElement);
+    return lastElement;
+}
+
+FindLastElement(['h','e','l','l','o']);
